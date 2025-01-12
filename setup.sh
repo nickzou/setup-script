706d0364bbec0fecfccfce54fe0f2d9942217f2f
Nick Zou <z.nick00@gmail.com>
2025-01-12 10:37:11 -0500

    add ubuntu debian version

--- setup.sh
+++ setup.sh
@@ -25,6 +25,14 @@ setup_package_manager() {
             log "Installing Homebrew..."
             /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         fi
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	    if [ -f "/etc/os-release" ]; then
+            . /etc/os-release
+            if [[ "$ID_LIKE" == *"debian"* ]] || [[ "$ID" == "debian" ]] || [[ "$ID" == "ubuntu" ]]; then
+                log "Updating apt repositories..."
+                sudo apt update
+            fi
+        fi
     fi
 }
 
@@ -38,6 +46,19 @@ main() {
     if has_command brew; then
         brew install neovim git lazygit tmux starship stow
 	brew install --cask ghostty
+    elif [ -f "/etc/os-release" ]; then
+        . /etc/os-release
+        if [[ "$ID_LIKE" == *"debian"* ]] || [[ "$ID" == "debian" ]] || [[ "$ID" == "ubuntu" ]]; then
+            sudo apt install -y neovim git lazygit tmux starship stow
+
+	    # Install lazygit
+            LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v*([^"]+)".*/\1/')
+            curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+            sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
+            rm lazygit.tar.gz
+
+	    lazygit --version
+        fi
     fi
     
     # Setup dotfiles
